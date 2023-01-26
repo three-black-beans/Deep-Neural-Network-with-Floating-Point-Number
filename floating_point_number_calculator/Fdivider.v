7663fc8388e20f62785fb0b28aa4e136da0443a7
three-black-beans <irudajam@naver.com>
2023-01-26 15:07:08 +0900

    fix: Fdivider import module name revision

--- floating_point_number_calculator/Fdivider.v
+++ floating_point_number_calculator/Fdivider.v
@@ -25,8 +25,8 @@
  *
  *---------------------------------------------------------------------------*/
 
-`include "adder.v"
-`include "multiplier.v"
+`include "Fadder.v"
+`include "Fmultiplier.v"
 module Fdivider(
     input [31:0] A,
     input [31:0] B,
@@ -39,28 +39,28 @@ wire [31:0] x0, x1, x2, x3, x4, x5, x6;
 wire [31:0] reciprocal;
 wire [7:0] exponent;
 
-multiplier init_multi (.A({1'b0, 8'd126, B[22:0]}), .B(32'b11000000000010110100101101001011), .reset_n(reset_n), .clk(clk), .result(temp1)); //verified
-adder init_sub (.A(32'h4034B4B5), .B({1'b1, temp1[30:0]}), .reset_n(reset_n), .clk(clk), .result(x0));
+Fmultiplier init_multi (.A({1'b0, 8'd126, B[22:0]}), .B(32'b11000000000010110100101101001011), .reset_n(reset_n), .clk(clk), .result(temp1)); //verified
+Fadder init_sub (.A(32'h4034B4B5), .B({1'b1, temp1[30:0]}), .reset_n(reset_n), .clk(clk), .result(x0));
 
-multiplier iterate_m1 (.A({{1'b0,8'd126,B[22:0]}}), .B(x0), .reset_n(reset_n), .clk(clk), .result(temp2));
-adder iterate_a1 (.A(32'h40000000), .B({!temp2[31], temp2[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp3));
-multiplier iterate_m2 (.A(x0), .B(temp3), .reset_n(reset_n), .clk(clk), .result(x1));
+Fmultiplier iterate_m1 (.A({{1'b0,8'd126,B[22:0]}}), .B(x0), .reset_n(reset_n), .clk(clk), .result(temp2));
+Fadder iterate_a1 (.A(32'h40000000), .B({!temp2[31], temp2[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp3));
+Fmultiplier iterate_m2 (.A(x0), .B(temp3), .reset_n(reset_n), .clk(clk), .result(x1));
 
-multiplier iterate_m3 (.A({1'b0,8'd126,B[22:0]}), .B(x1), .reset_n(reset_n),.clk(clk), .result(temp4));
-adder iterate_a2 (.A(32'h40000000), .B({!temp4[31], temp4[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp5));
-multiplier iterate_m4 (.A(x1), .B(temp5), .reset_n(reset_n), .clk(clk), .result(x2));
+Fmultiplier iterate_m3 (.A({1'b0,8'd126,B[22:0]}), .B(x1), .reset_n(reset_n),.clk(clk), .result(temp4));
+Fadder iterate_a2 (.A(32'h40000000), .B({!temp4[31], temp4[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp5));
+Fmultiplier iterate_m4 (.A(x1), .B(temp5), .reset_n(reset_n), .clk(clk), .result(x2));
 
-multiplier iterate_m5 (.A({1'b0,8'd126,B[22:0]}), .B(x2), .reset_n(reset_n), .clk(clk), .result(temp6));
-adder iterate_a3 (.A(32'h40000000), .B({!temp6[31],temp6[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp7));
-multiplier iterate_m6 (.A(x2), .B(temp7), .reset_n(reset_n), .clk(clk), .result(x3));
+Fmultiplier iterate_m5 (.A({1'b0,8'd126,B[22:0]}), .B(x2), .reset_n(reset_n), .clk(clk), .result(temp6));
+Fadder iterate_a3 (.A(32'h40000000), .B({!temp6[31],temp6[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp7));
+Fmultiplier iterate_m6 (.A(x2), .B(temp7), .reset_n(reset_n), .clk(clk), .result(x3));
 
-//multiplier iterate_m7 (.A({1'b0,8'd126,B[22:0]}), .B(x3), .reset_n(reset_n), .clk(clk), .result(temp8));
-//adder iterate_a4 (.A(32'h40000000), .B({!temp8[31],temp8[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp9));
-//multiplier iterate_m8 (.A(x3), .B(temp9), .reset_n(reset_n), .clk(clk), .result(x4));
+//Fmultiplier iterate_m7 (.A({1'b0,8'd126,B[22:0]}), .B(x3), .reset_n(reset_n), .clk(clk), .result(temp8));
+//Fadder iterate_a4 (.A(32'h40000000), .B({!temp8[31],temp8[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp9));
+//Fmultiplier iterate_m8 (.A(x3), .B(temp9), .reset_n(reset_n), .clk(clk), .result(x4));
 
-//multiplier iterate_m9 (.A({1'b0,8'd126,B[22:0]}), .B(x4), .reset_n(reset_n), .clk(clk), .result(tmp10));
-//adder iterate_a5 (.A(32'h40000000), .B({!temp10[31],temp10[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp11));
-//multiplier iterate_m10 (.A(x4), .B(temp11), .reset_n(reset_n), .clk(clk), .result(x5));
+//Fmultiplier iterate_m9 (.A({1'b0,8'd126,B[22:0]}), .B(x4), .reset_n(reset_n), .clk(clk), .result(tmp10));
+//Fadder iterate_a5 (.A(32'h40000000), .B({!temp10[31],temp10[30:0]}), .reset_n(reset_n), .clk(clk), .result(temp11));
+//Fmultiplier iterate_m10 (.A(x4), .B(temp11), .reset_n(reset_n), .clk(clk), .result(x5));
 
 /*----Reciprocal : 1/B----*/
 assign exponent = x3[30:23] + 8'd126 - B[30:23];
@@ -68,7 +68,7 @@ assign reciprocal = {B[31], exponent, x3[22:0]};
 
 
 /*----Multiplication A*1/B----*/
-multiplier multi_result (.A(A), .B(reciprocal), .reset_n(reset_n), .clk(clk), .result(x4));
+Fmultiplier multi_result (.A(A), .B(reciprocal), .reset_n(reset_n), .clk(clk), .result(x4));
 
 always @ (posedge clk) begin
     $display("%b x0", x0);
