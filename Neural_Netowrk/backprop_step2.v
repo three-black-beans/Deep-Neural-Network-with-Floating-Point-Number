c3f218b00179b54f4b9fcb81a0f08eec5c0c3b0b
three-black-beans <irudajam@naver.com>
2023-01-29 23:44:01 +0900

    fix: back propagation step 2 module revision

--- Neural_Netowrk/backprop_step2.v
+++ Neural_Netowrk/backprop_step2.v
@@ -7,7 +7,7 @@
  *               School of Electrical Engineering / Department of Biomechatronics
  *               Sungkyunkwan University
  *  Written on : January 18, 2023
- *  Version    : 2.0
+ *  Version    : 3.0
  *  Design     : Backpropagation step 2
  *  Target Devices: Zybo Z7-20
  *  Modification History:
@@ -20,23 +20,29 @@
  *      * January 26, 2023 by Jin Hyeong Park
  *        version 2.0 code modified and comments added.
  *
+ *      * January 29, 2023 by Jin Hyeong Park
+ *        version 3.0 released.
+ *
  *---------------------------------------------------------------------------*/
+ 
 
-
-module backprop_step2(
+module backprop_step2  #(parameter ONE = 32'b00111111100000000000000000000000)(
     input clk,
     input reset_n,
-    input [31:0] target, // 정답값
-    input [31:0] sigmoid_out, // 최종 output
-    input [31:0] out_value, // output_layer의 시그모이드 전 값
-    input [31:0] layer2_weight, // hidden_layer와 output_layer사이의 가중치
-    input [31:0] hidden_layer_value, // hidden_layer의 시그모이드 전 값
-    input [31:0] initial_input, // 처음 input값
-    input [31:0] initial_weight, // hidden_layer와 input_layer사이의 가중치
-    output [31:0] w_new //새로 업데이트 되는 가중치
+    input [31:0] target,             // 정답값
+    input [31:0] sigmoid_out,        // so1, 
+    input [31:0] out_value,          // o1,     output_layer의 시그모이드 전 값
+    input [31:0] layer2_weight,      // w10,     hidden_layer와 output_layer사이의 가중치
+    input [31:0] hidden_sigmoid_value,//h1,   hidden_layer의 시그모이드 값
+    input [31:0] hidden_layer_value, // z1,    hidden_layer의 시그모이드 전 값
+    input [31:0] initial_input,      // x1,   처음 input값
+    input [31:0] initial_weight,     // w1,   hidden_layer와 input_layer사이의 가중치
+    output [31:0] w_new              //w1+,    새로 업데이트 되는 가중치
 );
 
 wire [31:0] loss;
+wire [31:0] rev_so;
+wire [31:0] rev_h;
 wire [31:0] derv1_out;
 wire [31:0] hidden_out;
 wire [31:0] learning_mult;
@@ -47,19 +53,22 @@ wire [31:0] step3_mult;
 wire [31:0] step4_mult;
 
 // partial derivation start
-Fadder_Fsubtractor sub1 (.A(sigmoid_out), .B({1'b0, target[30:0]}), .clk(clk), .reset_n(reset_n), .result(loss)); // A - B
-sigmoid_deriv deriv1 (.deriv_object(out_value), .clk(clk), .reset_n(reset_n), .out(derv1_out));
-sigmoid_deriv deriv2 (.deriv_object(hidden_layer_value), .clk(clk), .reset_n(reset_n), .out(hidden_out));
 
+Fadder_Fsubtractor sub1 (.A(sigmoid_out), .B(target), .clk(clk), .IsSub(1'b1),.reset_n(reset_n), .result(loss)); // so1 - target
+Fadder_Fsubtractor sub2 (.A(ONE), .B(sigmoid_out),.IsSub(1'b1), .clk(clk), .reset_n(reset_n), .result(rev_so)); // 1 - so1     of    so1*(1-so1)
+Fadder_Fsubtractor sub3 (.A(ONE), .B(hidden_sigmoid_value),.IsSub(1'b1), .clk(clk), .reset_n(reset_n), .result(rev_h)); // 1 - h1  of  h1*(1-h1)
+
+Fmultiplier m01 (.A(sigmoid_out), .B(rev_so), .clk(clk), .reset_n(reset_n), .result(derv1_out)); // derv1_out = so1*(1-so1)
+Fmultiplier m02 (.A(hidden_sigmoid_value), .B(rev_h), .clk(clk), .reset_n(reset_n), .result(hidden_out)); // hidden_out = h*(1-h)
 
 Fmultiplier m1 (.A(loss), .B(derv1_out), .clk(clk), .reset_n(reset_n), .result(step1_mult));
 Fmultiplier m2 (.A(step1_mult), .B(layer2_weight), .clk(clk), .reset_n(reset_n), .result(step2_mult));
 Fmultiplier m3 (.A(step2_mult), .B(hidden_out), .clk(clk), .reset_n(reset_n), .result(step3_mult));
 Fmultiplier m4 (.A(step3_mult), .B(initial_input), .clk(clk), .reset_n(reset_n), .result(step4_mult));
 
-Fmultiplier m5 (.A(32'b00111110010011001100110011001101), .B(step4_mult), .clk(clk), .reset_n(reset_n), .result(learning_mult));
+Fmultiplier m5 (.A(32'b00111111000000000000000000000000), .B(step4_mult), .clk(clk), .reset_n(reset_n), .result(learning_mult)); // learning rate 0.5
 
-Fadder_Fsubtractor sub2 (.A(initial_weight), .B({1'b0, learning_mult[30:0]}), .clk(clk), .reset_n(reset_n), .result(w_new)); // A - B
+Fadder_Fsubtractor sub4 (.A(initial_weight), .B(learning_mult),.IsSub(1'b1), .clk(clk), .reset_n(reset_n), .result(w_new)); // A - B
 // partial derivation end
 
 
--- test_bench/backprop_step2_tb.v
+++ test_bench/backprop_step2_tb.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2023/01/25 18:28:54
+// Create Date: 2023/01/28 00:33:48
 // Design Name: 
 // Module Name: backprop_step2_tb
 // Project Name: 
@@ -32,6 +32,7 @@ reg [31:0] d;
 reg [31:0] e;
 reg [31:0] f;
 reg [31:0] g;
+reg [31:0] h;
 wire [31:0] r_oper;
 
 always begin : CLOCK_PULSE
@@ -40,15 +41,16 @@ end
 
 initial begin
     a = 32'b00111111010011001100110011001101;//정답값 = 0.8
-    b = 32'b00111111001101001011010010101111;//최종 output값인 0.705882
-    c = 32'b00111111001100110011001100110011;//output 값=0.7
+    b = 32'b00111111001100000000000000000000;//sigmoid_out=0.6875
+    c = 32'b00111111000110011001100110011010;//output 값=0.6
     d = 32'b00111111000000000000000000000000;//w10 가중치 = 0.5
-    e = 32'b00111111011001100110011001100110;//hidden_layer value=0.9
-    f = 32'b00111111000101000111101011100001;//처음 input = 0.58
-    g = 32'b00111111000110011001100110011010;//처음 가중치 = 0.6
+    e = 32'b00111111001100110011001100110011;//z1 =0.7
+    f = 32'b00111111000101000111101011100001;//x1 = 0.58
+    g = 32'b00111111000110011001100110011010;//w1 = 0.6
+    h = 32'b00111111001101001011010010101111;//sigmoid_hidden=0.705882
     reset_n = 1;
 end
 
-backprop_step2 bps(.clk(clk), .reset_n(reset_n), .target(a), .sigmoid_out(b), .out_value(c), .layer2_weight(d), .hidden_layer_value(e), .initial_input(f), .initial_weight(g), .w_new(r_oper));
+backprop_step2 bps(.clk(clk), .reset_n(reset_n), .target(a),.sigmoid_out(b),  .out_value(c), .layer2_weight(d), .hidden_layer_value(e), .initial_input(f), .initial_weight(g),.hidden_sigmoid_value(h), .w_new(r_oper));
 
 endmodule
