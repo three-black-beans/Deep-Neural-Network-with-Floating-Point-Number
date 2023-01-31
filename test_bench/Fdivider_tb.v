902c9dfeaa3fd6343acc93dce852f624af2a7487
three-black-beans <irudajam@naver.com>
2023-01-31 16:54:19 +0900

    add: input value file

--- test_bench/Fdivider_tb.v
+++ test_bench/Fdivider_tb.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2023/01/24 23:27:17
+// Create Date: 2023/01/23 15:39:54
 // Design Name: 
 // Module Name: Fdivider_tb
 // Project Name: 
@@ -27,18 +27,36 @@ reg [31:0] A;
 reg [31:0] B;
 reg reset_n;
 wire [31:0] result;
+reg [2:0] rand1, rand2, rand7, rand8;
+reg rand3, rand4;
+reg [4:0] temp;
+reg [19:0] temp2;
 
 always begin : CLOCK_PULSE
     #(CLOCK_PERIOD / 2) clk = ~clk;
 end
 
+always @(posedge clk) begin
+    rand1 = $urandom%7;
+    rand2 = $urandom%7;
+    rand3 = $urandom%1;
+    rand4 = $urandom%1;
+    temp = 5'b10000;
+    temp2 = 20'b10000000000000000000;
+    rand7 = $urandom%7;
+    rand8 = $urandom%7;
+    A = {rand3, temp, rand1, rand7, temp2};
+    B = {rand4, temp, rand2, rand8, temp2};
+    reset_n = 1;
+end
+
 initial begin
-    A = 32'b01000010111100000000000000000000; // 120
-    B = 32'b01000001001000000000000000000000; // 10
+    A = 32'b00000000000000000000000000000000; // 0
+    B = 32'b00000000000000000000000000000000; // 0
     reset_n = 1;
 end
 
 
-Fdivider divider_instance (.A(A), .B(B), .clk(clk), .reset_n(reset_n), .result(result));
+Fmultiplier multiplier_instance (.A(A), .B(B), .clk(clk), .reset_n(reset_n), .exception(exception), .result(result));
 
 endmodule
